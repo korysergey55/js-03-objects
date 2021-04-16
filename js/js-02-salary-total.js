fad900cba3e4e51dcf2f27e774dbc236ca7a7910
Serhiy Korytskyy <korysergey55@gmail.com>
2021-04-16 19:40:31 +0300

    objects

---
+++ js/js-02-salary-total.js
@@ -0,0 +1,20 @@
+
+// Напиши функцию которая принимает объект зарплат,
+
+// где имя свойства это имя сотрудника, а значение свойства это зарплата.
+
+// Функция должна рассчитать общую сумму зарплат сотрудников и вернуть её.
+
+// Используй переменную totalSalary для хранения общей суммы зарплаты.
+
+const countTotalSalary = function(salaries) {
+  let totalSalary = 0;
+  const arr = Object.values(salaries);
+  for (const key of arr) {
+    totalSalary += key;
+  }
+console.log(totalSalary)
+  return totalSalary;
+}
+countTotalSalary({ mango: 100, poly: 150, alfred: 80 }); //возвращает 330.
+countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }); // возвращает 400.
